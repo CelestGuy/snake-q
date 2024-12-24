0cdeed641b8cfa2bba878a6ac3fe08ee1b7e59f6
Théo Duval <theo.duval27@proton.me>
2024-12-24 23:25:41 +0100

    test

--- js/q-learning.js
+++ js/q-learning.js
@@ -116,6 +116,8 @@ class QLearning {
     }
 
     train() {
+        this.initQTable();
+
         this.training = true;
         const ran = document.getElementById("a");
         const ran2 = document.getElementById("ab");
@@ -229,4 +231,9 @@ function getState(board) {
     }
 
     return state;
+}
+
+onmessage = function(event) {
+    console.log(JSON.stringify(event.data));
+    postMessage({ data: "Bien reçu !" });
 }
